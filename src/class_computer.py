12acbbd2d49a56fe83eb2897daa05dd166021c61
saullas <majsavli@gmail.com>
2020-05-26 18:34:52 +0200

    class_computer works, four link_prediction methods for now

--- src/class_computer.py
+++ src/class_computer.py
@@ -1,7 +1,17 @@
 import networkx as nx
 import pandas as pd
-from random import choice, sample
+import numpy as np
+
+from cdlib.algorithms import leiden
+
+from math import factorial
+from random import choice
+from itertools import combinations_with_replacement
+
 from src.scraper.scraper import get_graph
+
+
+N_OF_RUNS = 5
 
 def compute_auc(indexes, m, sampleSize):
 	m1, m2 = 0, 0
@@ -13,11 +23,11 @@ def compute_auc(indexes, m, sampleSize):
 			m1 += 1
 		elif randLPs == randLNs:
 			m2 += 1
-	return round((m1+m2/2)/(m/10), 3)
+	return (m1+m2/2)/(m/10)
 
-def evaluate_link_prediction_method(G: nx.Graph, method, negative, positive, sampleSize):
+def evaluate_link_prediction_method(G: nx.Graph, m, method, negative, positive, sampleSize):
 	indexes = compute_indexes(G, method, negative, positive)
-	return compute_auc(indexes, G.number_of_edges(), sampleSize)
+	return compute_auc(indexes, m, sampleSize)
 
 def compute_indexes(G: nx.Graph, method, negative, positive):
 	if method == 'resource_allocation_index':
@@ -28,21 +38,30 @@ def compute_indexes(G: nx.Graph, method, negative, positive):
 		return nx.adamic_adar_index(G,negative), nx.adamic_adar_index(G,positive)
 	elif method == 'preferential_attachment':
 		return nx.preferential_attachment(G,negative), nx.preferential_attachment(G,positive)
+	elif method == 'cn_soundarajan_hopcroft':
+		return nx.cn_soundarajan_hopcroft(G,negative), nx.cn_soundarajan_hopcroft(G,positive)
+	elif method == 'ra_index_soundarajan_hopcroft':
+		return nx.ra_index_soundarajan_hopcroft(G,negative), nx.ra_index_soundarajan_hopcroft(G,positive)
+	elif method == 'within_inter_cluster':
+		return nx.within_inter_cluster(G,negative), nx.within_inter_cluster(G,positive)
 	else:
-		return -1
+		raise NameError('The given method is not supported')
+
+def unconnected_pairs(G: nx.Graph):
+	all_pairs = set(combinations_with_replacement(G.nodes, 2))		
+	return list(all_pairs - set(G.edges))
 
 def compute_negative_and_positive_pairs(G: nx.Graph):
 	m = G.number_of_edges()
 	sampleSize = int(m * 0.1)
-	negative = []
-	pairs = 0
-	nodes = list(G.nodes)
-	while pairs < sampleSize:
-		[rand1,rand2] = sample(nodes,2)
-		if not connected(G,rand1,rand2) and (rand1,rand2) not in negative and (rand2,rand1) not in negative:
-			negative.append((rand1,rand2))
-			pairs += 1
-	positive = sample(list(G.edges()),sampleSize)
+	
+	unconnected = np.array(unconnected_pairs(G))
+	indexes = np.random.choice(len(unconnected), size=sampleSize, replace=False)
+	negative = unconnected[indexes]
+	
+	connected = np.array(list(G.edges))
+	indexes = np.random.choice(len(connected), size=sampleSize, replace=False)
+	positive = connected[indexes]
 	for edge in positive:
 		G.remove_edge(edge[0],edge[1])
 	return sampleSize, negative, positive
@@ -50,16 +69,44 @@ def compute_negative_and_positive_pairs(G: nx.Graph):
 def connected(G: nx.Graph, u, v):
 	return u in G.neighbors(v)
 
+def community_index(G: nx.Graph, i, j, commLabels, comms):
+	ci, cj = comms[i][0], comms[j][0]
+	if ci != cj:
+		return 0
+	C = commLabels[ci]
+	nc, mc = len(C), len(nx.subgraph(G,C))
+	return mc/(factorial(nc)/(2*factorial(nc-2)))
+
 networks_df = pd.read_csv('data/precomputed.csv')
 link_prediction_methods = ['resource_allocation_index',
 													'jaccard_coefficient',
 													'adamic_adar_index',
-													'preferential_attachment']
+													'preferential_attachment',]
+
+
 
 for index, row in networks_df.iterrows():
-	G = get_graph(row['name'], row['download_url'])
-	scores = {}
-	sampleSize, negative, positive = compute_negative_and_positive_pairs(G.copy())
-	for method in link_prediction_methods:
-		scores[method] = evaluate_link_prediction_method(G, method, negative, positive, sampleSize)
-	print(row['name'], scores)
+	originalG = get_graph(row['name'], row['download_url'])
+	# average scores
+	scores = {method: 0 for method in link_prediction_methods}
+	m = originalG.number_of_edges()
+	
+	for run in range(N_OF_RUNS):
+		print(f'------- Run {run} -------')
+		G = originalG.copy()
+		sampleSize, negative, positive = compute_negative_and_positive_pairs(G)
+		
+		for method in link_prediction_methods:
+			successful = False
+			while not successful:
+				try:
+					scores[method] += evaluate_link_prediction_method(G, m, method, negative, positive, sampleSize) / N_OF_RUNS
+					successful = True
+				except: # division by 0 when computing adamic adar
+					print(method, 'not successful...')
+					# we compute negative and positive pairs again
+					G = originalG.copy()
+					sampleSize, negative, positive = compute_negative_and_positive_pairs(G)
+		
+	print(f'Network number {index}, scores: {[(method,round(score,3)) for method,score in scores.items()]}')
+	
