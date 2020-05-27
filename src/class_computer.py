397119dce48e21dd6aaf8bd206b19f08d1602dad
saullas <majsavli@gmail.com>
2020-05-27 13:41:16 +0200

    class computer works

--- src/class_computer.py
+++ src/class_computer.py
@@ -4,15 +4,52 @@ import numpy as np
 
 from cdlib.algorithms import leiden, louvain
 
-from math import factorial
+from math import factorial, isnan
 from random import choice
 from itertools import combinations_with_replacement
 
 from src.scraper.scraper import get_graph
 
+def compute_class(G: nx.Graph):
+	# average scores
+	scores = {method: 0 for method in link_prediction_methods}
+	m = G.number_of_edges()
 
-N_OF_RUNS = 5
+	for run in range(N_OF_RUNS):
+		print(f'------- Run {run} -------')
+		G_test = G.copy()
+		sampleSize, negative, positive = compute_negative_and_positive_pairs(G_test)
+		
+		for method in link_prediction_methods:
+			successful = False
+			while not successful:
+				try:
+					scores[method] += compute_auc(G_test, m, method, negative, positive, sampleSize) / N_OF_RUNS
+					successful = True
+				except Exception as inst:
+					print(f'{method} failed. {type(inst)}, {inst.args}')
+					# we compute negative and positive pairs again
+					G_test = G.copy()
+					sampleSize, negative, positive = compute_negative_and_positive_pairs(G_test)
+	
+	print(f'Network {index}, scores: {[(method,round(score,3)) for method,score in scores.items()]} \n')
+	# return class - index of the best method
+	aucs = list(scores.values())
+	return aucs.index(max(aucs))
 
+def compute_auc(G:nx.Graph, m, method, negative, positive, sampleSize):
+	neg_indexes, pos_indexes = compute_indexes(G, method, negative, positive)
+	m1, m2 = 0, 0
+	neg_indexes, pos_indexes = list(neg_indexes), list(pos_indexes)
+	
+	for i in range(sampleSize):
+		_,_,rand_neg = choice(neg_indexes)
+		_,_,rand_pos = choice(pos_indexes)
+		if rand_pos > rand_neg:
+			m1 += 1
+		elif rand_pos == rand_neg:
+			m2 += 1
+	return (m1+m2/2)/(m/10)
 
 def compute_negative_and_positive_pairs(G: nx.Graph):
 	m = G.number_of_edges()
@@ -28,20 +65,6 @@ def compute_negative_and_positive_pairs(G: nx.Graph):
 	for edge in positive:
 		G.remove_edge(edge[0],edge[1])
 	return sampleSize, negative, positive
-
-def compute_auc(G:nx.Graph, m, method, negative, postivie, sampleSize):
-	neg_indexes, pos_indexes = compute_indexes(G, method, negative, positive)
-	m1, m2 = 0, 0
-	neg_indexes, pos_indexes = list(neg_indexes), list(pos_indexes)
-	
-	for i in range(sampleSize):
-		_,_,rand_neg = choice(neg_indexes)
-		_,_,rand_pos = choice(pos_indexes)
-		if rand_pos > rand_neg:
-			m1 += 1
-		elif rand_pos == rand_neg:
-			m2 += 1
-	return (m1+m2/2)/(m/10)
 
 def compute_indexes(G: nx.Graph, method, negative, positive):
 	if method == 'resource_allocation':
@@ -83,6 +106,7 @@ def sorensen_index(G: nx.Graph, i, j):
 	return len(i_neighbourhood.intersection(j_neighbourhood))/degreeSum
 
 
+
 def unconnected_pairs(G: nx.Graph):
 	all_pairs = set(combinations_with_replacement(G.nodes, 2))		
 	return list(all_pairs - set(G.edges))
@@ -96,9 +120,6 @@ def remove_weights(G: nx.Graph):
 			del G.edges[edge]['weight']
 
 
-networks_df = pd.read_csv('data/precomputed.csv')
-classes = []
-
 link_prediction_methods = ['resource_allocation',
 													 'jaccard_coefficient',
 													 'adamic_adar',
@@ -107,31 +128,18 @@ link_prediction_methods = ['resource_allocation',
 													 'sorensen_neighbours']
 
 
-for index, row in networks_df.iterrows():
-	G_original = get_graph(row['name'], row['download_url'])
-	remove_weights(G_original)
-	
-	# average scores
-	scores = {method: 0 for method in link_prediction_methods}
-	m = G_original.number_of_edges()
-	
-	for run in range(N_OF_RUNS):
-		print(f'------- Run {run} -------')
-		G = G_original.copy()
-		sampleSize, negative, positive = compute_negative_and_positive_pairs(G)
-		
-		for method in link_prediction_methods:
-			successful = False
-			while not successful:
-				try:
-					scores[method] += compute_auc(G, m, method, negative, positive, sampleSize) / N_OF_RUNS
-					successful = True
-				except Exception as inst:
-					print(f'{method} not successful. {type(inst)}, {inst.args}')
-					# we compute negative and positive pairs again
-					G = G_original.copy()
-					sampleSize, negative, positive = compute_negative_and_positive_pairs(G)
-		
-	print(f'Network {index}, scores: {[(method,round(score,3)) for method,score in scores.items()]} \n')
-	aucs = list(scores.values())
-	classes.append(aucs.index(max(aucs))) # append index of the best value as class
+N_OF_RUNS = 5
+OVERWRITE = False # if we compute and overwrite class where its already been computed
+SAVE_RATE = 20 # save every n networks
+
+if __name__ == '__main__':
+	networks_df = pd.read_csv('data/precomputed_with_classes.csv')
+
+	for index, row in networks_df.iterrows():
+		if isnan(row['Class']) or OVERWRITE:
+			G = get_graph(row['name'], row['download_url'])
+			remove_weights(G)
+			classVariable = compute_class(G)
+			networks_df.iloc[index, networks_df.columns.get_loc('Class')] = classVariable
+		if index % SAVE_RATE == 0: # save every 20 networks
+			networks_df.to_csv('data/precomputed_with_classes.csv', index=False)
