a7663e01ab38b01534bb91223e2aa0b5369c7424
saullas <majsavli@gmail.com>
2020-06-19 15:43:12 +0200

    classifier1 fix

---
+++ src/classifier1.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing, neighbors
+from sklearn.ensemble import BaggingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.feature_selection import VarianceThreshold
+from statistics import mean, stdev
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from random import randint, seed
+
+best_avg_method = 0 # resource alloc best avg method
+classifiers = ['SVC', 'KNN', 'RNDF']
+link_prediction_methods = ['resource_allocation',
+													 'jaccard_coefficient',
+													 'preferential_attachment',
+													 'community',
+													 'sorensen_neighbours']
+N_OF_RUNS = 10 # number of times we build the model
+
+def info(runScores):
+  for classifier,scores in zip(classifiers,runScores):
+    print(f'{classifier} mean CA: {round(mean(scores)*100,3)}, stdev: {round(stdev(scores)*100,3)}')
+
+if __name__ == '__main__':
+  dataset = pd.read_csv('data/precomputed_with_classes_3.csv')
+  target = dataset['class'].astype(int)
+  aucs = dataset.iloc[:,-5:].to_numpy()
+  dataset.drop(['r','name','download_url']+link_prediction_methods, 1, inplace=True)
+  dataset = pd.get_dummies(dataset) # one hot encode categorical attributes
+
+  cor = dataset.corr()
+  cor_target = abs(cor['class'])
+  relevant_features = cor_target[cor_target > 0.03]
+  #print(relevant_features)
+
+  selected_columns = ['C_avg']
+  dataset.drop(dataset.columns.difference(selected_columns), 1, inplace=True)
+
+  scores = [[] for i in range(len(classifiers))] # scores for every method and run
+  seed(42) # for reproducability
+
+  for run in range(N_OF_RUNS):
+    print(f'Run {run}/{N_OF_RUNS}')
+    X_train, X_test, y_train, y_test = train_test_split(dataset,target,test_size=0.2,random_state=randint(0,10000))
+
+    model = SVC()
+    model.fit(X_train, y_train)
+    scores[0].append(model.score(X_test,y_test))
+
+    model2 = neighbors.KNeighborsClassifier(n_neighbors=3)
+    model2.fit(X_train, y_train)
+    scores[1].append(model2.score(X_test,y_test))
+
+    model3 = RandomForestClassifier(n_estimators=100)
+    model3.fit(X_train, y_train)
+    scores[2].append(model3.score(X_test,y_test))
+
+  info(scores)
